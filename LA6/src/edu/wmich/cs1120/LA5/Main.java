e031b4bd704e576bc1bbc4c35a5957488eb6174c
Michael <Michael@LAPTOP-FNO4A9K3>
2018-11-21 13:25:43 -0500

    Michael Coffey just copied and pasted what instructions put for main

--- LA6/src/edu/wmich/cs1120/LA5/Main.java
+++ LA6/src/edu/wmich/cs1120/LA5/Main.java
@@ -3,7 +3,13 @@ package edu.wmich.cs1120.LA5;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		IEncoder encoder = new Encoder();
+		IDecoder decoder = new Decoder();
+		String inputFileName = "input.txt";
+		String encodedFileName = inputFileName+".encode";
+		encoder.encode(inputFileName,encodedFileName);
+		decoder.decode(encodedFileName);
 
 	}
 
