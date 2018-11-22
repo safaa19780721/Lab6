c20fb0705bbb20ec3813ed311d98e0609cfcaba5
Michael <Michael@LAPTOP-FNO4A9K3>
2018-11-21 20:09:27 -0500

    Michael Coffey added comments too decoder for javadoc

--- LA6/src/edu/wmich/cs1120/LA5/Decoder.java
+++ LA6/src/edu/wmich/cs1120/LA5/Decoder.java
@@ -5,13 +5,23 @@ import java.io.RandomAccessFile;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author MichaelCoffey&DylanTarnacki
+ * This class is used to decode a binary file and print the message on screen
+ *
+ */
 public class Decoder implements IDecoder {
 	
+	/**
+	 * decode is used to read the file, decode the message, then print the new message on screen. Uses randomAccessFile and
+	 * a while loop to search through the file. Finds a char then an int after. Skips how many spots the int says. Then get next char.
+	 * Will throw an exception if the file can't be found.
+	 */
 	public void decode(String filePath) {
 		String lineInFile = "";
 		String msg = "";
 		int distance = 0;
-		char repeatedChar;
 		try {
 			RandomAccessFile in = new RandomAccessFile(filePath, "rw");
 			while(distance != -1) {
